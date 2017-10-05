838a83dc1a42d783c3eb8e2d8cfe0656e4813669
Christopher Dubeau <Christopherdubeau@gmail.com>
2017-10-05 16:05:48 -0600

    Fixed Search layout and Added Functionality to search_main.xml

--- app/src/main/java/com/example/ericlam/moviesearcherapp/SearchActivity.java
+++ app/src/main/java/com/example/ericlam/moviesearcherapp/SearchActivity.java
@@ -2,7 +2,10 @@ package com.example.ericlam.moviesearcherapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 
@@ -10,14 +13,29 @@ import android.widget.Spinner;
  * Created by ChrisDubeau on 2017-10-05.
  */
 public class SearchActivity extends AppCompatActivity {
-    public String[] Genre = new String[]{"Horror","Comedy","Western"};
-    public String[] Alphabet = new String[]{"A","B","C"};
+    public String[] genre = new String[]{"Horror","Comedy","Western"};
+    public String[] year = new String[10];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_main);
         Spinner spinner = (Spinner) findViewById(R.id.genreDrop);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,R.layout.support_simple_spinner_dropdown_item,Genre);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,R.layout.support_simple_spinner_dropdown_item,genre);
         spinner.setAdapter(adapter);
+        Spinner spinner2 = (Spinner) findViewById(R.id.yearDrop);
+        for(int i = 1; i <= 10; i++){
+            year[i-1] = Integer.toString(i);
+        }
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this,R.layout.support_simple_spinner_dropdown_item,year);
+        spinner2.setAdapter(adapter2);
+        final Button advButton = (Button) findViewById(R.id.advSearch);
+        advButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinearLayout advOption = (LinearLayout) findViewById(R.id.advOption);
+                advOption.setVisibility(View.VISIBLE);
+                advButton.setVisibility(View.GONE);
+            }
+        });
     }
 }
